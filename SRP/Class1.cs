2f06986e212340fa692de29219c5d2e56c0d539f
xavier415 <xaviercolon061@gmail.com>
2024-08-02 20:12:39 -0400

    Add project files.

---
+++ SRP/Class1.cs
@@ -0,0 +1,31 @@
+﻿
+namespace SRP
+{
+    
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Direccion: {Direccion}");
+            Console.WriteLine($"Correo: {CorreoElectronico}");
+        }
+    }
+
+    
+    public class ServicioCorreo
+    {
+        public void EnviarCorreoElectronico(string correoElectronico, string mensaje)
+        {
+            // Lógica para enviar correo electrónico
+            Console.WriteLine($"Enviando correo a {correoElectronico} con el mensaje: {mensaje}");
+        }
+    }
+
+}
